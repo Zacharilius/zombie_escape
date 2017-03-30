31e88f8eb09734c71c53bef273acc6400e102e2c
Zacharilius <zabensley@gmail.com>
2017-03-29 21:27:56 -0700

    Refactor reorganize game.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def main():
 
     size = [constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT]
     screen = pygame.display.set_mode(size)
-
     pygame.display.set_caption('Zombie Escape')
 
     player = Player()
@@ -20,16 +19,14 @@ def main():
     player.rect.y = constants.SCREEN_HEIGHT - player.rect.height
     active_sprite_list.add(player)
 
-    done = False
-
     clock = pygame.time.Clock()
 
+    done = False
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-
-            if event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     player.go_north()
                 elif event.key == pygame.K_RIGHT:
@@ -38,22 +35,17 @@ def main():
                     player.go_south()
                 elif event.key == pygame.K_LEFT:
                     player.go_west()
-
-            if event.type == pygame.KEYUP:
+            elif event.type == pygame.KEYUP:
                 player.stop()
 
         active_sprite_list.update()
 
-        screen.fill(constants.BLUE)
+        screen.fill(constants.WHITE)
         active_sprite_list.draw(screen)
-
 
         clock.tick(65)
 
         pygame.display.flip()
-
-    pygame.quit()
-
 
 if __name__ == "__main__":
     main()
