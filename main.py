f113548e53b8b9c21265850f77c0b0b84960702d
Zacharilius <zabensley@gmail.com>
2017-04-05 20:14:45 -0700

    Created an Enemy class. Created end game screen.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import settings
 import pygame
 from pygame.mixer import Sound
-from pygame.sprite import groupcollide
+from pygame.sprite import groupcollide, spritecollide
 import random
 import sys
 
@@ -16,15 +16,19 @@ def main():
     window = pygame.display.set_mode(size)
     pygame.display.set_caption('Zombie Escape')
 
-    # ==== Init Screen ====
-
-    if len(sys.argv) >= 2 and sys.argv[1] == 'skip_intro':
+    if 'skip_intro' in sys.argv:
         show_intro_screen = False
     else:
         show_intro_screen = True
+    if 'mute_sound' in sys.argv:
+        mute_sound = True
+    else:
+        mute_sound = False
+
+    # ==== Intro Screen ====
 
     intro_title_font_size = 60
-    intro_title_font = pygame.font.Font('assets/jonathan-s-harris_something-strange/something_strange.ttf', intro_title_font_size)  # Default system font
+    intro_title_font = pygame.font.Font('assets/jonathan-s-harris_something-strange/something_strange.ttf', intro_title_font_size)
     intro_font_title_image = intro_title_font.render('Brains... it\'s what\'s for dinner', True, settings.RED)
 
     intro_screen_show_time = 5000 # 5 seconds
@@ -40,9 +44,13 @@ def main():
 
     player_sprite_list = pygame.sprite.Group()
     player = Player(wall_sprite_list)
-    player.rect.x = 340
-    player.rect.y = settings.SCREEN_HEIGHT - player.rect.height
     player_sprite_list.add(player)
+
+    enemy_sprite_group = pygame.sprite.Group()
+    enemy1 = Enemy(x=30, y=30, direction='horizontal', collision_sprite_group=wall_sprite_list)
+    enemy_sprite_group.add(enemy1)
+    enemy2 = Enemy(x=settings.SCREEN_WIDTH / 2, y=30, direction='vertical', collision_sprite_group=wall_sprite_list)
+    enemy_sprite_group.add(enemy2)
 
     brain_sprite_list = pygame.sprite.Group()
     brains_eaten = 0
@@ -62,7 +70,15 @@ def main():
     brain_eating_sound = Sound('assets/zombie_brain_eating.ogg')
 
     pygame.mixer.music.load('assets/game_music.ogg')
-    pygame.mixer.music.play(-1, 0.0)  # -1 Causes to loop indefinitely
+    if not mute_sound:
+        pygame.mixer.music.play(-1, 0.0)  # -1 Causes to loop indefinitely
+
+    # ==== End Screen ====
+
+    show_end_screen = False
+    end_game_title_font_size = 60
+    end_game_title_font = pygame.font.Font('assets/jonathan-s-harris_something-strange/something_strange.ttf', end_game_title_font_size)
+    end_game_font_title_image = end_game_title_font.render('You died...', True, settings.RED)
 
     done = False
     while not done:
@@ -70,7 +86,9 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                if event.key == pygame.K_ESCAPE:
+                    done = True
+                elif event.key == pygame.K_UP or event.key == pygame.K_w:
                     player.go_north()
                 elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                     player.go_east()
@@ -91,23 +109,26 @@ def main():
         window.fill(settings.WHITE)
 
         if show_intro_screen:
-            window.blit(intro_font_title_image, (settings.SCREEN_WIDTH / 2 - intro_font_title_image.get_width() / 2, settings.SCREEN_WIDTH / 3))
+            window.blit(intro_font_title_image, (settings.SCREEN_WIDTH / 2 - intro_font_title_image.get_width() / 2, settings.SCREEN_HEIGHT / 2))
 
             if pygame.time.get_ticks() > intro_screen_show_time:
                 show_intro_screen = False
-
+        elif show_end_screen:
+            window.blit(end_game_font_title_image, (settings.SCREEN_WIDTH / 2 - end_game_font_title_image.get_width() / 2, settings.SCREEN_HEIGHT / 3))
         else:
             time_since_last_brain_created += clock.get_time()
             ten_seconds = 10000
             if time_since_last_brain_created > ten_seconds:
-                x, y = random.randint(0, settings.SCREEN_WIDTH), random.randint(0, settings.SCREEN_HEIGHT)
+                edge_offset = 20
+                x, y = random.randint(0 + edge_offset, settings.SCREEN_WIDTH - edge_offset), random.randint(0 + edge_offset, settings.SCREEN_HEIGHT - edge_offset)
                 brain = Brain(x, y)  # TODO pass in x, y
                 brain_sprite_list.add(brain)
                 time_since_last_brain_created = 0
 
             for brain in groupcollide(brain_sprite_list, player_sprite_list, True, False, collided=None):
                 brains_eaten += 1
-                brain_eating_sound.play()
+                if not mute_sound:
+                    brain_eating_sound.play()
 
             background_single_sprite_group.update()
             background_single_sprite_group.draw(window)
@@ -118,11 +139,17 @@ def main():
             player_sprite_list.update()
             player_sprite_list.draw(window)
 
+            enemy_sprite_group.update()
+            enemy_sprite_group.draw(window)
+
             brain_sprite_list.update()
             brain_sprite_list.draw(window)
 
             brains_eaten_message_image = get_brains_eaten_message_image(brains_left_font, brains_eaten)
             window.blit(brains_eaten_message_image, (settings.SCREEN_WIDTH - brains_eaten_message_image.get_width() - 20, 20))
+
+            if spritecollide(player, enemy_sprite_group, True, collided=None):
+                show_end_screen = True
 
         clock.tick(65)
 
@@ -137,7 +164,7 @@ class SpriteSheet(object):
     sprite_sheet = None
 
     def __init__(self, file_name, num_columns, num_rows):
-        self.sprite_sheet = pygame.image.load(file_name).convert()
+        self.sprite_sheet = pygame.image.load(file_name).convert_alpha()
         self.num_columns = num_columns
         self.num_rows = num_rows
 
@@ -154,7 +181,8 @@ class SpriteSheet(object):
         return self.sprite_sheet.get_height()
 
     def get_image(self, x, y, width, height):
-        image = pygame.Surface([width, height]).convert()
+        image = pygame.Surface([width, height])
+        image.fill(settings.BLACK)
         image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
         image.set_colorkey(settings.BLACK)
         return image
@@ -194,7 +222,6 @@ class Player(pygame.sprite.Sprite):
     moving_south = False
     moving_west = False
 
-
     def __init__(self, collision_sprite_group):
         super().__init__()
 
@@ -209,6 +236,8 @@ class Player(pygame.sprite.Sprite):
 
         self.image = self.walking_frames_n[0]
         self.rect = self.image.get_rect()
+        self.rect.x = 340
+        self.rect.y = settings.SCREEN_HEIGHT - self.rect.height - self.speed
 
     def update(self):
         pos_x = self.rect.x - self.change_x
@@ -297,6 +326,82 @@ class Player(pygame.sprite.Sprite):
         self.moving_west = False
         self.change_x = 0
 
+
+class Enemy(pygame.sprite.Sprite):
+    change_x = 0
+    change_y = 0
+    speed = 18
+
+    moving_north = False
+    moving_east = False
+    moving_south = True
+    moving_west = False
+
+    def __init__(self, x, y, direction, collision_sprite_group):
+        super().__init__()
+
+        self.sprite_sheet = SpriteSheet('assets/doubleleggy_sprites/sprite_sheet.png', num_columns=12, num_rows=8)
+
+        self.walking_frames_n = self.sprite_sheet.get_frames(col_start_index=9, row_start_index=3, sprite_cols=3)
+        self.walking_frames_e = self.sprite_sheet.get_frames(col_start_index=9, row_start_index=2, sprite_cols=3)
+        self.walking_frames_s = self.sprite_sheet.get_frames(col_start_index=9, row_start_index=0, sprite_cols=3)
+        self.walking_frames_w = self.sprite_sheet.get_frames(col_start_index=9, row_start_index=1, sprite_cols=3)
+
+        self.collision_sprite_group = collision_sprite_group
+
+        if direction == 'horizontal':
+            self.moving_east = True
+            self.image = self.walking_frames_e[0]
+            self.change_x = 20
+        elif direction == 'vertical':
+            self.moving_south = True
+            self.image = self.walking_frames_s[0]
+            self.change_y = 20
+        else:
+            raise 'Did not recognize: %s' % direction
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        pos_x = self.rect.x - self.change_x
+        pos_y = self.rect.y
+
+        if spritecollide(self, self.collision_sprite_group, False, collided=None):
+            self.reverse_direction()
+
+        frame_multiplier = 6
+        if self.moving_north:
+            frame = (pos_y * frame_multiplier // self.sprite_sheet.get_sprite_width()) % len(self.walking_frames_n)
+            self.image = self.walking_frames_n[frame]
+        elif self.moving_east:
+            frame = (pos_x * frame_multiplier // self.sprite_sheet.get_sprite_height()) % len(self.walking_frames_e)
+            self.image = self.walking_frames_e[frame]
+        elif self.moving_south:
+            frame = (pos_y * frame_multiplier // self.sprite_sheet.get_sprite_height()) % len(self.walking_frames_s)
+            self.image = self.walking_frames_s[frame]
+        elif self.moving_west:
+            frame = (pos_x * frame_multiplier // self.sprite_sheet.get_sprite_width()) % len(self.walking_frames_w)
+            self.image = self.walking_frames_w[frame]
+
+        self.image = pygame.transform.scale(self.image, (50, 50))
+
+        self.rect.x += self.change_x
+        self.rect.y += self.change_y
+
+    def reverse_direction(self):
+        # Switch east/west direction
+        old_east = self.moving_east
+        self.moving_east = self.moving_west
+        self.moving_west = old_east
+        self.change_x = -self.change_x
+
+        # Rotate north/south direction
+        old_south = self.moving_south
+        self.moving_south = self.moving_north
+        self.moving_north = old_south
+        self.change_y = -self.change_y
 
 class Wall(pygame.sprite.Sprite):
 
